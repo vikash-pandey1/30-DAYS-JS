244c60dd60992c8272b7bf5d3da497e866767bac
vikash-pandey1 <vikashpandey7082@gmail.com>
2024-07-14 15:17:32 +0530

    day1 complete of variables and data types

---
+++ Day1_variable.js
@@ -0,0 +1,30 @@
+var no = 12;
+let str = "ram"
+console.log(no)
+console.log(str)
+
+const fal = false;
+console.log(fal)
+
+const n = 10;
+const s = 'rama'
+const b = true;
+const obj = {
+    1:'ram',
+    2:'shyam'
+}
+const arr = [1,2,3,'f',4]
+console.log(typeof(n))
+console.log(typeof(s))
+console.log(typeof(b))
+console.log(typeof(obj))
+console.log(typeof(arr))
+
+let i = 1
+i = 2
+console.log(i)
+
+const ass = 23
+console.log(ass)
+ass =25
+console.log(ass)
